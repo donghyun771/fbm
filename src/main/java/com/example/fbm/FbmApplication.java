82a09d5a02ad5820ef9e95c5d4dae3ab36fa1881
donghyun771 <donghyun761@gmail.com>
2022-09-21 16:07:08 +0900

    Initial commit

---
+++ src/main/java/com/example/fbm/FbmApplication.java
@@ -0,0 +1,13 @@
+package com.example.fbm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbmApplication.class, args);
+	}
+
+}
