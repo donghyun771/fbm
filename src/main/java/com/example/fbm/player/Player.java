e255b9f5b83cbba90e138d66b313398e524a2a53
donghyun771 <donghyun761@gmail.com>
2022-09-23 17:44:35 +0900

    player 클래스

---
+++ src/main/java/com/example/fbm/player/Player.java
@@ -0,0 +1,34 @@
+package com.example.fbm.player;
+
+public class Player {
+
+    private long player_id;
+
+    private String player_nm;
+
+    private String backNum;
+
+    public long getPlayer_id() {
+        return player_id;
+    }
+
+    public void setPlayer_id(long player_id) {
+        this.player_id = player_id;
+    }
+
+    public String getPlayer_nm() {
+        return player_nm;
+    }
+
+    public void setPlayer_nm(String name) {
+        player_nm = name;
+    }
+
+    public String getBackNum() {
+        return backNum;
+    }
+
+    public void setBackNum(String backNum) {
+        this.backNum = backNum;
+    }
+}
