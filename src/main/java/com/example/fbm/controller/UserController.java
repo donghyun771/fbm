c0add891c2f3596e98df84aacdd97ac091771c92
donghyun771 <donghyun761@gmail.com>
2022-10-29 17:51:38 +0900

    로그인 및 회원가입 추가

---
+++ src/main/java/com/example/fbm/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.example.fbm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
+}
